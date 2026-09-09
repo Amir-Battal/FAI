f4fef1e73aff6cab7b3c40f1e72d3b41c5e69705
Amir-Battal <ameerbatttal@hotmail.com>
2026-09-09 15:04:15 +0300

    update Images

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -81,7 +81,7 @@ export default function LoadingScreen({ onComplete }) {
         <div className="w-75 h-155 flex items-center justify-center overflow-hidden ">
           <video
             ref={videoRef}
-            src="https://res.cloudinary.com/dqe36doqn/video/upload/v1781541163/liquidLogo_1_gj6s9v.mp4"
+            src="https://res.cloudinary.com/c9tptioy/video/upload/v1788955441/liquidLogo_1.mp4"
             muted
             playsInline
             loop={false}
